76a7d8f1567a05dd4778dcb00b86665ec3950bec
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-11-01 23:01:58 +0700

    finish modelling users

--- jbi_backend/accounts/admin.py
+++ jbi_backend/accounts/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import JBIDetails, UserDetails
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+
+class UserDetailsInline(admin.StackedInline):
+    model = UserDetails
+    can_delete = False
+    verbose_name_plural = "userdetails"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserDetailsInline,)
+
+
+class JBIDetailsAdmin(admin.ModelAdmin):
+    fields = ("nama_lembaga", "email_lembaga", "diluar_jadwal", "is_nonactive")
+    list_display = (
+        "nama_lembaga",
+        "email_lembaga",
+        "diluar_jadwal",
+        "is_nonactive",
+        "last_updated",
+    )
+
+
+class UserDetailsAdmin(admin.ModelAdmin):
+    fields = (
+        "user",
+        "is_activated",
+        "role",
+        "phone",
+        "gender",
+        "user_photo",
+        "last_updated",
+    )
+    list_display = (
+        "user",
+        "is_activated",
+        "role",
+        "phone",
+        "gender",
+        "last_updated",
+    )
+
+
+admin.site.register(JBIDetails, JBIDetailsAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserDetails, UserDetailsAdmin)
--- jbi_backend/accounts/models.py
+++ jbi_backend/accounts/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class JBIDetails(models.Model):
+    nama_lembaga = models.CharField(
+        max_length=100, blank=True, editable=True, default=None, null=True
+    )
+    email_lembaga = models.EmailField(
+        blank=True, editable=True, default=None, null=True
+    )
+    last_updated = models.DateTimeField(auto_now=True)
+    diluar_jadwal = models.BooleanField(default=False, editable=True)
+    is_nonactive = models.BooleanField(default=False, editable=True)
+
+
+class UserDetails(models.Model):
+    USER_ROLE = (
+        ("JBI", "JBI"),
+        ("USER", "USER"),
+        ("ADMIN", "ADMIN"),
+    )
+    GENDER_OPTIONS = (
+        ("L", "L"),
+        ("P", "P"),
+        ("-", "-"),
+    )
+    is_activated = models.BooleanField(default=False, editable=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    role = models.CharField(
+        max_length=10,
+        choices=USER_ROLE,
+        default="USER",
+        editable=True,
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_OPTIONS,
+        default="-",
+        editable=True,
+    )
+    phone = models.CharField(max_length=15, blank=True)
+    user_photo = models.ImageField(blank=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_details(sender, instance, created, **kwargs):
+    if created:
+        UserDetails.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_details(sender, instance, **kwargs):
+    instance.userdetails.save()
