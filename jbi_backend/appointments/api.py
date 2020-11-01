662b70f71ce021991fda688e3645c02d98af0ac8
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-11-02 03:06:07 +0700

    finish all get user details api

--- jbi_backend/appointments/api.py
+++ jbi_backend/appointments/api.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 
 class AppointmentViewset(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = AppointmentSerializer
 
     def get_queryset(self):
@@ -16,9 +16,35 @@ class AppointmentViewset(viewsets.ModelViewSet):
         serializer.save(user_creator=self.request.user)
 
 
-class GetJBIViewset(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
-    serializer_class = GetJBISerializer
+class GetJBIViewset(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        return JBIDetails.objects.all()
+    def get(self, request):
+        det_list = []
+
+        users = User.objects.all()
+        for u in users:
+            details = UserDetails.objects.get(user=u)
+            d = {
+                "role": details.role,
+                "name": u.username,
+                "phone": details.phone,
+                "email": u.email,
+                "gender": details.gender,
+                "is_activated": details.is_activated,
+            }
+            if details.role == "JBI":
+                jbi_details = JBIDetails.objects.get(jbi_username=u.username)
+                d = {
+                    "role": details.role,
+                    "name": u.username,
+                    "lembaga": jbi_details.nama_lembaga,
+                    "phone": details.phone,
+                    "email": u.email,
+                    "gender": details.gender,
+                    "is_activated": details.is_activated,
+                    "is_available": not jbi_details.is_nonactive,
+                    "diluar_jadwal": jbi_details.diluar_jadwal,
+                }
+            det_list.append(d)
+        return Response(det_list)
--- jbi_backend/appointments/urls.py
+++ jbi_backend/appointments/urls.py
@@ -4,9 +4,8 @@ from .api import AppointmentViewset, GetJBIViewset
 
 router = routers.DefaultRouter()
 router.register("api/v1/appointments", AppointmentViewset, "appointments")
-router.register("api/v1/get-jbi-list", GetJBIViewset, "JBIlist")
 
 urlpatterns = [
-    # path("api/v1/get-jbi-list", GetJBIViewset.as_view()),
+    path("api/v1/get-jbi-list", GetJBIViewset.as_view()),
 ]
 urlpatterns += router.urls
