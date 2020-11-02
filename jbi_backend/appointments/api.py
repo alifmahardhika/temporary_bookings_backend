9158164f49777f8f0344b3eb28cd76934ca0210f
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-11-02 15:22:20 +0700

    update jbi list api

--- jbi_backend/appointments/api.py
+++ jbi_backend/appointments/api.py
@@ -20,19 +20,23 @@ class GetJBIViewset(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        details = UserDetails.objects.get(role="JBI")
-        user = details.user
-        jbi_details = JBIDetails.objects.get(jbi_username=user.username)
-        return Response(
-            {
-                "user_id": user.id,
-                "name": user.username,
-                "lembaga": jbi_details.nama_lembaga,
-                "phone": details.phone,
-                "email": user.email,
-                "gender": details.gender,
-                "is_available": not jbi_details.is_nonactive,
-                "diluar_jadwal": jbi_details.diluar_jadwal,
-                "note": jbi_details.note,
-            }
-        )
+        detail_list = UserDetails.objects.filter(role="JBI")
+
+        result = []
+        for details in detail_list:
+            user = details.user
+            jbi_details = JBIDetails.objects.get(jbi_username=user.username)
+            result.append(
+                {
+                    "user_id": user.id,
+                    "name": user.username,
+                    "lembaga": jbi_details.nama_lembaga,
+                    "phone": details.phone,
+                    "email": user.email,
+                    "gender": details.gender,
+                    "is_available": not jbi_details.is_nonactive,
+                    "diluar_jadwal": jbi_details.diluar_jadwal,
+                    "note": jbi_details.note,
+                }
+            )
+        return Response(result)
